c25a90780377b9981229de9996bf1b21305a6c18
lai2-01 <lai2-01@itismeucci.com>
2021-09-27 11:40:48 +0200

    socket

---
+++ src/main/java/it/galli/App.java
@@ -0,0 +1,16 @@
+package it.galli;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
