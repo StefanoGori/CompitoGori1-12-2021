c778587ed12fa730bd4683dbce6a92fe741567e7
lai2-01 <lai2-01@itismeucci.com>
2021-12-01 12:50:04 +0100

    non funzionante

--- server/Serverstr.java
+++ server/Serverstr.java
@@ -34,30 +34,24 @@ public class Serverstr{
     public void comunica(){
         try{
             for(;;){
-                System.out.println(b+" bella");
-                System.out.println(b+" mando la richiesta del numero");
-                outVersoClient.writeBytes(b+" Dammi il numero estratto"+'\n');
+                outVersoClient.writeBytes(" Dammi il numero estratto"+'\n');
                 //rimango in attesa della riga trasmessa dal client
                 stringaRicevuta= inDalClient.readLine();
-                System.out.println(b+" ricevuto numero");
                 System.out.println(stringaRicevuta);
                 //trasformo la stringa in int
                 stringaricevuta=Integer.parseInt(stringaRicevuta);
-                System.out.println(b+" stringa trasformata in int");
+                System.out.println("stringa trasformata in int");
                 a.add(stringaricevuta);
-                System.out.println(b+" size vettore: "+a.size());
+                System.out.println(" size vettore: "+a.size());
                 for(int i=0;i<a.size();i++){
-                    System.out.println(b+" entro nel for");
                     if(stringaricevuta==a.get(i)){
-                        System.out.println(b+" if numero gia presente");
-                        outVersoClient.writeBytes(b+" ERRORE: numero gia' presente"+'\n');
+                        outVersoClient.writeBytes("ERRORE: numero gia' presente"+'\n');
                         break;
                     }
                     else{
-                        System.out.println(b+" entro nell'else");
                         a.add(stringaricevuta);
                         Collections.sort(a);
-                        System.out.println(b+" stringa dal cliente: "+stringaRicevuta);
+                        System.out.println(" stringa dal cliente: "+stringaRicevuta);
                         outVersoClient.writeBytes(a+" I numeri estratti sono: "+'\n');
                         for(i=0;i<a.size();i++){
                             outVersoClient.writeBytes(a.get(i)+"-");
@@ -69,11 +63,9 @@ public class Serverstr{
                                 client.close();
                             }
                         }
-                        System.out.println(b+" esco dall'else");
                         break;
                     }
                 }
-                System.out.println(b+" fuori for");
                 b++;
             }
         }
